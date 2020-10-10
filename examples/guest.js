e3bb9f0c97474f85f37da07a976955b797348c0a
Superscripts <anonymous.superscripts@gmail.com>
2020-10-10 14:07:15 +0200

    docs(examples): :memo: update examples
    update examples to match current state of the module

--- examples/guest.js
+++ examples/guest.js
@@ -1,24 +1,41 @@
 // Require the module locally. You will need to require 'ib-helper' instead!
 const ib = require('../dist');
 
-// DISCLAIMER: Using the API as guest user is significantly slower! Use proper credentials instead!
+// DISCLAIMER: Using the API as guest user can be significantly slower! Use proper credentials instead!
 
 async function main() {
   // Instantiate helper class
   const helper = new ib.Helper();
 
   // Login (left blank for guest access)
+  /* SESSION MANAGEMENT included! (even refreshing) */
   await helper.login();
 
-  // Adjust ratings to show mature content
+  // Adjust ratings to only show sfw content
   await helper.rating({
-    nudity: true,
-    violence: true,
-    sexualThemes: true,
-    strongViolence: true,
+    nudity: false,
+    violence: false,
+    sexualThemes: false,
+    strongViolence: false,
   });
 
-  // ...
+  const submissions = [];
+
+  /* PREWRITTEN FUNCTIONS for common use cases! */
+  const firstPage = await helper.searchTags(['the_lion_king', 'simba']);
+  submissions.push(...firstPage.submissions);
+
+  /* PAGINATION FUNCTIONS for easier page iteration! */
+  const secondPage = await firstPage.nextPage();
+  submissions.push(...secondPage.submissions);
+
+  // Format results
+  const titles = submissions.map((s) => s.title);
+
+  // Output submission titles
+  titles.forEach((title) => {
+    console.log(title);
+  });
 
   // Logout to make sure the session gets invalidated
   await helper.logout();
--- examples/guest.ts
+++ examples/guest.ts
@@ -1,24 +1,41 @@
 // Import the module locally. You will need to import from 'ib-helper' instead!
-import Helper from '../src';
+import Helper, { Submission } from '../src';
 
-// DISCLAIMER: Using the API as guest user is significantly slower! Use proper credentials instead!
+// DISCLAIMER: Using the API as guest user can be significantly slower! Use proper credentials instead!
 
 async function main() {
   // Instantiate helper class
   const helper = new Helper();
 
   // Login (left blank for guest access)
+  /* SESSION MANAGEMENT included! (even refreshing) */
   await helper.login();
 
-  // Adjust ratings to show mature content
+  // Adjust ratings to only show sfw content
   await helper.rating({
-    nudity: true,
-    violence: true,
-    sexualThemes: true,
-    strongViolence: true,
+    nudity: false,
+    violence: false,
+    sexualThemes: false,
+    strongViolence: false,
   });
 
-  // ...
+  const submissions: Submission[] = [];
+
+  /* PREWRITTEN FUNCTIONS for common use cases! */
+  const firstPage = await helper.searchTags(['the_lion_king', 'simba']);
+  submissions.push(...firstPage.submissions);
+
+  /* PAGINATION FUNCTIONS for easier page iteration! */
+  const secondPage = await firstPage.nextPage();
+  submissions.push(...secondPage.submissions);
+
+  // Format results
+  const titles = submissions.map((s) => s.title);
+
+  // Output submission titles
+  titles.forEach((title) => {
+    console.log(title);
+  });
 
   // Logout to make sure the session gets invalidated
   await helper.logout();
